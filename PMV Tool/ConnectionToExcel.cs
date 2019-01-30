ff4f631a5eadd9e0209a4a6accbc7b34fdd56ae0
Matt Trussler <meberem@gmail.com>
2019-01-30 15:29:13 +0000

    Copy code from zip

---
+++ PMV Tool/ConnectionToExcel.cs
@@ -0,0 +1,36 @@
+﻿using LinqToExcel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMV_Tool
+{
+    public class ConnexionExcel
+    {
+        public string _pathExcelFile;
+        public ExcelQueryFactory _urlConnexion;
+        public ConnexionExcel(string path)
+
+        {
+            this._pathExcelFile = path;
+            this._urlConnexion = new ExcelQueryFactory(_pathExcelFile);
+        }
+        public string PathExcelFile
+        {
+            get
+            {
+                return _pathExcelFile;
+            }
+        }
+        public ExcelQueryFactory UrlConnexion
+        {
+            get
+            {
+                return _urlConnexion;
+            }
+        }
+
+    }
+}
---
+++ PMV Tool/ExcelCategories.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMV_Tool
+{
+    class ExcelCategories
+    {
+        public int Materials
+        {
+            get;
+            set;
+        }
+
+        public int Labour
+        {
+            get;
+            set;
+        }
+        public int PlantAndTemporaryWorks
+        {
+            get;
+            set;
+        }
+
+        public int siteSupervision
+        {
+            get;
+            set;
+        }
+
+        public double UniqueID
+        {
+            get;
+            set;
+        }
+        public string ScenarioNumber
+        {
+            get;
+            set;
+        }
+    }
+}
+
